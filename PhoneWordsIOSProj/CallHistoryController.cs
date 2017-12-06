70641fd7aa7e3916554068bfb2fdfb1214050645
masonmil <masonis@me.com>
2017-12-06 05:11:33 -0500

    Initial check-in of module PhoneWordsIOSSln

---
+++ PhoneWordsIOSProj/CallHistoryController.cs
@@ -0,0 +1,72 @@
+using Foundation;
+using System;
+using UIKit;
+using System.Collections.Generic;
+
+namespace PhoneWordsIOSProj
+{
+    public partial class CallHistoryController : UITableViewController
+    {
+
+        public List<string> PhoneNumbers { get; set; }
+
+        static NSString callHistoryCellId = new NSString("CallHistoryCell");
+
+        public CallHistoryController(IntPtr handle) : base(handle)
+        {
+            TableView.RegisterClassForCellReuse(typeof(UITableViewCell), callHistoryCellId);
+            TableView.Source = new CallHistoryDataSource(this);
+            PhoneNumbers = new List<string>();
+
+        }
+
+        class CallHistoryDataSource : UITableViewSource
+        {
+            CallHistoryController controller;
+            public CallHistoryDataSource(CallHistoryController controller)
+            {
+                this.controller = controller;
+            }
+
+            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+            {
+                UIAlertController okAlertController = UIAlertController.Create("Row Selected", controller.PhoneNumbers[indexPath.Row], UIAlertControllerStyle.Alert);
+                okAlertController.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                controller.PresentViewController(okAlertController, true, null);
+
+            }
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                var cell = tableView.DequeueReusableCell(CallHistoryController.callHistoryCellId);
+                var row = indexPath.Row;
+                cell.TextLabel.Text = controller.PhoneNumbers[row];
+                return cell;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                return controller.PhoneNumbers.Count;
+            }
+
+            public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+            {
+                switch (editingStyle)
+                {
+                    case UITableViewCellEditingStyle.Delete:
+                        controller.PhoneNumbers.RemoveAt(indexPath.Row);
+                        tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+                        break;
+                    case UITableViewCellEditingStyle.None:
+                        Console.WriteLine("CommitEditingStyle.None called");
+                        break;
+                }
+            }
+
+            public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath) => true;
+
+
+
+        }
+
+    }
+}
---
+++ PhoneWordsIOSProj/ViewController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+
+namespace PhoneWordsIOSProj
+{
+    public partial class ViewController : UIViewController
+    {
+        string translatedNumber = string.Empty;       
+
+        public List<string> PhoneNumbers { get; set; }
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            //initialize list of phone numbers called for Call History screen
+            PhoneNumbers = new List<string>();
+
+        }
+#region ViewDidLoad
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+
+            TranslateButton.TouchUpInside += (object sender, EventArgs e) => {
+
+                translatedNumber = PhoneTranslator.ToNumber(
+                    PhoneNumberText.Text);
+
+                PhoneNumberText.ResignFirstResponder();
+
+                if (translatedNumber == string.Empty)
+                {
+                    CallButton.SetTitle("Call ", UIControlState.Normal);
+                    CallButton.Enabled = false;
+                }
+                else{
+                    
+                
+                    CallButton.SetTitle("Call "+ translatedNumber, UIControlState.Normal);
+                    CallButton.Enabled = true;
+                }
+
+            };
+
+            CallButton.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                var url = new NSUrl("tel:" + translatedNumber);
+                PhoneNumbers.Add(translatedNumber);
+
+                if (!UIApplication.SharedApplication.OpenUrl(url))
+                {
+                    var alert = UIAlertController.Create("Not Supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                    alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+                    PresentViewController(alert, true, null);
+
+                }
+            };
+        }
+#endregion
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            // set the View Controller that’s powering the screen we’re
+            // transitioning to
+
+            var callHistoryContoller = segue.DestinationViewController as CallHistoryController;
+
+            //set the Table View Controller’s list of phone numbers to the
+            // list of dialed phone numbers
+
+            if (callHistoryContoller != null)
+            {
+                callHistoryContoller.PhoneNumbers = PhoneNumbers;
+            }
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
