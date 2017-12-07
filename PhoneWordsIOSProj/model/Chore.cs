41768c69ed02d8d0499263fdbbc462a7db974ccc
masonmil <masonis@me.com>
2017-12-07 16:20:51 -0500

    chore model

---
+++ PhoneWordsIOSProj/model/Chore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PhoneWordsIOSProj.model
+{
+
+    public class Chore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+    }
+}
