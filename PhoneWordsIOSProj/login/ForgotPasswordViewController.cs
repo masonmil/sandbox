09adeae223c0a70bebde6f6b4075145cc6bdb072
masonmil <masonis@me.com>
2017-12-08 07:18:58 -0500

    fixed error

--- PhoneWordsIOSProj/login/ForgotPasswordViewController.cs
+++ PhoneWordsIOSProj/login/ForgotPasswordViewController.cs
@@ -6,10 +6,7 @@ namespace PhoneWordsIOSProj
 {
     public partial class ForgotPasswordViewController : UIViewController
     {
-        partial void Cancel_TouchCancel(UIButton sender)
-        {
-            throw new NotImplementedException();
-        }
+
 
         public ForgotPasswordViewController(IntPtr handle) : base(handle)
         {
